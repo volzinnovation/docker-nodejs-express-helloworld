15f8de21b9fd2ba36d481a6e817a488af70f9c34
Raphael Volz <rv@volzinnovation.com>
2020-10-13 23:14:44 +0000

    Added Raw Headers

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('<h1>Hello World</h1><p>' + req.rawHeaders + '</p>');
 });
 
 app.listen(PORT, HOST);
